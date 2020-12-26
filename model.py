e931b907249aaf5db367d3b3490a0225c228904e
Matias Luraghi <matiaslgh@gmail.com>
2020-12-26 16:43:58 -0300

    Use a generator

--- model.py
+++ model.py
@@ -3,7 +3,13 @@ import cv2
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
+import sklearn
+from sklearn.model_selection import train_test_split
+from math import ceil
 
+BATCH_SIZE = 30
+CORRECTION = 0.17
+IMAGES_PER_CSV_LINE = 6 # Left, Center, Right + their flipped versions
 DATA_DIRECTORY = './simulator-data/IMG/'
 
 def get_current_path(path):
@@ -21,8 +27,6 @@ def get_images_with_measurements(line):
     right_img = read_img_rgb(line[2])
     measurement = float(line[3])
 
-    CORRECTION = 0.17
-
     return [
         (center_img, measurement),
         (left_img, measurement + CORRECTION),
@@ -32,18 +36,33 @@ def get_images_with_measurements(line):
         (np.fliplr(right_img), -measurement + CORRECTION),
     ]
 
-images =  []
-measurements = []
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    batch_size_pre_augmentated = ceil(batch_size / IMAGES_PER_CSV_LINE)
+    # Loop forever so the generator never terminates
+    while True:
+        np.random.shuffle(samples)
+        for offset in range(0, num_samples, batch_size_pre_augmentated):
+            batch_samples = samples[offset:offset + batch_size_pre_augmentated]
 
+            images =  []
+            measurements = []
+
+            for batch_sample in batch_samples:
+                for img, measurement in get_images_with_measurements(line):
+                    images.append(img)
+                    measurements.append(measurement)
+
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+samples = []
 with open('./simulator-data/driving_log.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        for img, measurement in get_images_with_measurements(line):
-            images.append(img)
-            measurements.append(measurement)
-
-X_train = np.array(images)
-y_train = np.array(measurements)
+        samples.append(line)
 
 model = Sequential()
 model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
@@ -51,6 +70,18 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5))
 model.add(Flatten())
 model.add(Dense(1))
 
+# compile and train the model using the generator function
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+train_generator = generator(train_samples, BATCH_SIZE)
+validation_generator = generator(validation_samples, BATCH_SIZE)
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
+model.fit_generator(
+    train_generator,
+    steps_per_epoch= ceil(len(train_samples) * IMAGES_PER_CSV_LINE / BATCH_SIZE),
+    validation_data=validation_generator,
+    validation_steps=ceil(len(validation_samples) * IMAGES_PER_CSV_LINE / BATCH_SIZE),
+    epochs=1,
+    verbose=1
+)
 model.save('model.h5')
